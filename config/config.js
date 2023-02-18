09c16a58df52144017ba591f2083f260e5f2abb8
wujunlin <wjllance@163.com>
2023-02-18 14:55:56 +0800

    base

--- config/config.js
+++
@@ -1,7 +0,0 @@
-exports.config = {
-  wechat: {
-    appID: '*************',
-    appsecret: '**********',
-    token: 'hellozheng'
-  }
-}
--- controller/handleEvent.js
+++ controller/handleEvent.js
@@ -1,33 +1,50 @@
-var xml2js = require('xml2js')
-var tools = require('../config/tools.js')
+var xml2js = require("xml2js");
+var tools = require("../config/tools.js");
+var { ChatGPTAPI } = import("chatgpt");
+const { config } = require("../config/config.js");
 
 // 处理用户发送过来的文字消息和点击事件
 exports.msg = (req, res) => {
-  var xml = '' //存储将要回复给公众号的文字
-  var json = null //用于存储xml转换为json
+  var xml = ""; //存储将要回复给公众号的文字
+  var json = null; //用于存储xml转换为json
 
   //接收post内容
-  req.on('data', chunk => { 
-    xml += chunk
-  })
+  req.on("data", (chunk) => {
+    xml += chunk;
+  });
 
   //接收结束
-  req.on('end', () => {
+  req.on("end", () => {
     //将接受到的xml数据转化为json
-    xml2js.parseString(xml, { explicitArray: false }, function(err, json) {
-      var backTime = new Date().getTime() //创建发送时间，整数
+    xml2js.parseString(xml, { explicitArray: false }, function (err, json) {
+      var backTime = new Date().getTime(); //创建发送时间，整数
+
+      console.log("json", json);
 
       //event表示事件,
-      if (json.xml.MsgType == 'event') {
+      if (json.xml.MsgType == "event") {
         // EventKey是在自定义菜单的时候定义的事件名称
-        if (json.xml.EventKey == 'clickEvent') {
-          res.send(tools.getXml(json, backTime, '你戳我干啥...'))
+        if (json.xml.EventKey == "clickEvent") {
+          res.send(tools.getXml(json, backTime, "你戳我干啥..."));
         }
         //text表示文字信息
-      } else if (json.xml.MsgType == 'text') {
-        //回复公众号的文字信息
-        res.send(tools.getXml(json, backTime, `你发"${json.xml.Content}"过来干啥？`))
+      } else if (json.xml.MsgType == "text") {
+        import("chatgpt").then(({ ChatGPTAPI }) => {
+          const api = new ChatGPTAPI({
+            apiKey: config.openai.apiKey,
+            debug: true,
+          });
+
+          api.sendMessage(json.xml.Content).then((ans) => {
+            console.log(json.xml.Content, "res:", ans.text);
+            //回复公众号的文字信息
+            res.send(
+              // tools.getXml(json, backTime, `你发"${json.xml.Content}"过来干啥？`)
+              tools.getXml(json, backTime, ans.text)
+            );
+          });
+        });
       }
-    })
-  })
-}
+    });
+  });
+};
